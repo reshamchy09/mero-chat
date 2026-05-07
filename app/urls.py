ef0dfee0f32503f62e6b4c0dd2083c0ebac00222
Resham Chaudhary <reshamchy2004@gmail.com>
2026-05-07 21:06:52 +0545

    Updated app templates features and fixed bugs

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import latest_update
+from .views import app_update_list, latest_update
 
 urlpatterns = [
     path('api/latest-update/', latest_update),
+        path('', app_update_list, name='app_list'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import AppUpdate
 from .serializers import AppUpdateSerializer
+from django.shortcuts import render
 
 @api_view(['GET'])
 def latest_update(request):
     latest = AppUpdate.objects.order_by('-version_code').first()
     serializer = AppUpdateSerializer(latest)
     return Response(serializer.data)
+
+def app_update_list(request):
+    apps = AppUpdate.objects.all().order_by('-version_code')
+    return render(request, 'app_list.html', {'apps': apps})
